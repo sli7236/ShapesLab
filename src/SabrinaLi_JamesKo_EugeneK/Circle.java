98b8e8fed8cfaa4e5c64c25bb280714a6a981d8c
BT_1N3_14 <sli7236@bths.edu>
2018-10-02 11:33:22 -0400

    circle

--- src/SabrinaLi_JamesKo_EugeneK/Circle.java
+++ src/SabrinaLi_JamesKo_EugeneK/Circle.java
@@ -1,4 +1,22 @@
 package SabrinaLi_JamesKo_EugeneK;
 
-public class Circle {
+public class Circle extends Shape {
+
+    private int radius;
+    public Circle (int radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius * radius * Math.PI ;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * radius * Math.PI;
+    }
+
+
 }
--- src/SabrinaLi_JamesKo_EugeneK/RPentagon.java
+++ src/SabrinaLi_JamesKo_EugeneK/RPentagon.java
@@ -6,6 +6,7 @@ public class RPentagon extends Shape{
         super("RPentagon");
         this.side = side;
     }
+
     @Override
     public double getArea() {
         return 0.25*Math.sqrt(5*(5+2*Math.sqrt(5)))*(side*side);
